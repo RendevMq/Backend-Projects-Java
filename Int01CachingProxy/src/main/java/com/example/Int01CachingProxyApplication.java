602717c7783344c8ac11af3d5a10f0adbee9d66e
RendevMq <rensystem73@gmail.com>
2024-09-22 23:35:32 -0500

    dependencies

---
+++ Int01CachingProxy/src/main/java/com/example/Int01CachingProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Int01CachingProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Int01CachingProxyApplication.class, args);
+	}
+
+}
